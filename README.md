0d8f661c0aeb6c05ce6165208233e39c2fe30666
yagizkaymak <yagiz.kaymak@gmail.com>
2019-02-14 10:06:30 -0500

    Comments have been added

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
 # Table of Contents
+1. [Author](README.md#author)
 1. [Introduction](README.md#introduction)
+1. [Introduction](README.md#dataset)
+1. [Input](README.md#input)
+1. [Output](README.md#output)
+1. [Running The Code](README.md#running-the-code)
+1. [Test Cases](README.md#test-cases)
 
-# Introduction
+
+## Author
+This project developed by Yagiz Kaymak using Python 2.7 to solve Edgar Analytics coding challenge at Insight Data Science.
+The code is also accessible online with the following link:
+https://github.com/yagizkaymak/edgar-analytics
+
+February 2019
+
+## Introduction
+Many investors, researchers, journalists and others use the Securities and Exchange Commission's (SEC's) Electronic Data Gathering, Analysis and Retrieval (EDGAR) system to retrieve financial documents, whether they are doing a deep dive into a particular company's financials or learning new information that a company has revealed through their filings.
+
+This project uses EDGAR weblogs to identify the webpage visits of users as sessions. A session is identified as all webpage visits within a time interval.
+It is assumed that each line of EDGAR weblogs represents a single web request for an EDGAR document that would be streamed into our application in real time.
+
+Using the provided EDGAR weblogs data, the goal of this project is to output the user sessions. Each user session should include the duration and the number of documents requested during that visit, and then write the output to a file.
+
+## Dataset
+The SEC maintains EDGAR weblogs showing which IP addresses have accessed which documents for what company, and at what day and time this occurred.
+
+A sample EDGAR weblog file is stored in "input" directory with the filename of "log.csv".
+"log.csv" is a comma separated file with a header as its first line. These header fields are listed as follows:
+
+*ip, date, time, zone, cik, accession, extention, code, size, idx, norefer, noagent, find, crawler, browser*
+
+In this project we only use the following fields related to the challenge:
+
+ip: identifies the IP address of the device requesting the data. While the SEC anonymizes the last three digits, it uses a consistent formula that allows you to assume that any two ip fields with the duplicate values are referring to the same IP address
+date: date of the request (yyyy-mm-dd)
+time: time of the request (hh:mm:ss)
+cik: SEC Central Index Key
+accession: SEC document accession number
+extention: Value that helps determine the document being requested
+
+It is assumed that the combination of cik, accession, and extention fields
+uniquely identifies a single web page document request.
+
+
+## Input
+Two input files, "log.csv" and "inactivity_period.txt", exists in the "input" folder. As mentioned in Dataset section "log.csv" file
+includes EDGAR weblogs in a comma separated format.
+"inactivity_period.txt" file holde a single integer value denoting the period of inactivity (in seconds) that the
+"edgar.analytics.py" script usee to identify a user session. The value can range from 1 to 86,400 (i.e., one second to 24 hours).
+
+## Output
+The output of this project is created as a text file, called "sessionization.txt", and stored in the "output" folder.
+"sessionization.txt" includes the results with a comma separated format as follows:
+
+IP address of the user exactly as found in log.csv
+date and time of the first webpage request in the session (yyyy-mm-dd hh:mm:ss)
+date and time of the last webpage request in the session (yyyy-mm-dd hh:mm:ss)
+duration of the session in seconds
+count of webpage requests during the session
+
+Once your "edgar_analytics.py" script identifies the start and end of a session, it gathers the mentioned fields and write them out to a line in the output file, "sessionization.txt". The fields on each line are separated by a comma.
+
+
+## Running The Code
+In order to run the code provided in this repository, please use the shell script "run.sh" located in the root folder of this project.
+
+
+## Test Cases
+Six test cases can be found under "test" folder. "test_1" is the test case created by Insight Data Science. "test_x", where x denotes a number from 2 to 6, are different test cases created by Yagiz Kaymak.
+
+To run the test cases please run "./run_tests.sh" bash script in the "test" folder.
--- test/temp/src/edgar_analytics.py
+++ test/temp/src/edgar_analytics.py
@@ -1,5 +1,12 @@
+"""
+This is the code that reads the EDGAR weblogs and inactivity_time.txt,
+detects the sessions and outputs the results in 'sessionization.txt' file
+
+Author: Yagiz Kaymak
+February, 2019
+"""
+
 import csv
-import datetime
 import date_utils as du
 import operator
 import sys
@@ -7,6 +14,7 @@ import sys
 TIMESTAMP_FORMAT = "YYYY-mm-dd HH:MM:SS"
 
 def read_inactivity_time(filename):
+    """ Reads the incactivity_time stored in 'inactivity_time.txt' file """
     with open(filename,'r') as f:
         line = f.readline()
         inactivity_time = line.rstrip('\n')
@@ -14,8 +22,11 @@ def read_inactivity_time(filename):
 
 
 def read_weblog(filename):
-    """ Reads the log file and extract the related fields,
-    such as IP address, access date, access time """
+    """
+    Reads the log file (log.csv) and extract the related fields,
+    such as IP address, access date, access time.
+    Extracted fields are returned as a list
+    """
     line_number = 0
     weblog_list = list()
 
@@ -29,41 +40,52 @@ def read_weblog(filename):
 
 
 def main():
-
-    #log_filename = "../input/log.csv"
+    # Get the filename of log file as the first command line argument
     log_filename = sys.argv[1]
 
+    # Read the log file from "log.csv" and store it in
+    # a list called weblog_list
     weblog_list = read_weblog(log_filename)
 
-    #inactivity_period_filename = "../input/inactivity_period.txt"
+    # Read the inactivity time from "inactivity_time.txt" and store it in
+    # a variable called inactivity_time
     inactivity_period_filename = sys.argv[2]
     inactivity_time = int(read_inactivity_time(inactivity_period_filename))
 
-    #sessinization_file_handler = open('../output/sessionization.txt','w+')
+    # Get the sessionization file handler to append the lines to "sessionization" whenever it is necessary.
     sessinization_file_handler = open(sys.argv[3],'w+')
 
     # Dictionary to store IP sessions
     event_log_dic = {}
+
+    # Index that will be used to identify the line number of the log file
+    # when we itarate through the weblog_list that stores the logs
     index = 1
 
+    # Iterate through the weblog_list
     for log in weblog_list:
 
-        date_time_str = log[1] + ' ' + log[2] # Concatenate date and time
+        # Concatenate date and time
+        date_time_str = log[1] + ' ' + log[2]
         date_time = du.get_string_as_datetime(date_time_str)
 
-        # If this IP address does no exist in the dictionary, this is its first apperance
-        # so create an entry for it. Initial values are first access = current_time,
-        # last access = current_time, web page access count = 1
+        # If this IP address does no exist in the dictionary, this is its first apperance.
+        # So create an entry for it. Initial values are
+        # first access = current_time, last access = current_time, web page access count = 1
         if not log[0] in event_log_dic:
             event_log_dic[log[0]] = [date_time, date_time, 1]
 
-        # If this IP exists in the dictionary, update its last access time and increment its web page access count
+        # If this IP exists in the dictionary, update its last access time and increment its web page access count by one
         else:
             event_log_dic[log[0]][1] = date_time # Update this IP's last access time with current time
             event_log_dic[log[0]][2] = event_log_dic[log[0]][2] + 1 # Increment web page access count by one
 
-        # Stores sessions to be removed
-        to_be_removed = list()
+
+        # Inactive sessions will be identified every time when we add or modify
+        # the ictionary.
+        # inactive_sessions dictionary will store the inactive sessions to be removed from the
+        # the IP dictionary that stores the identified sessions so far.
+        inactive_sessions = list()
 
         # Check if all any IP's session expired
         for IP in event_log_dic:
@@ -84,10 +106,10 @@ def main():
                 # Prepare the output format and append it to the sessionization.txt
                 output = IP + ',' + du.get_datetime_as_str(first_webpage_request_datetime) + ',' + du.get_datetime_as_str(last_webpage_request_datetime) + ',' + str(duration_of_the_session) + ',' + str(count_of_webpage_requests)
                 sessinization_file_handler.write("%s\n" % output)
-                to_be_removed.append(IP)
+                inactive_sessions.append(IP)
 
-        # Remove all IP with expired sessions from the IP dictionary
-        for IP in to_be_removed:
+        # Remove all IPs with expired sessions from the IP dictionary
+        for IP in inactive_sessions:
             del event_log_dic[IP]
 
 
@@ -95,8 +117,10 @@ def main():
         if index == len(weblog_list):
 
             # To break the ties with same start time we sort them according to their values
+            # values = (first access time, last access time, session duration, and web page count)
             sorted_event_log_list = sorted(event_log_dic.items(), key=operator.itemgetter(1))
 
+            # Itarate through the sorted IP list
             for IP in sorted_event_log_list:
                 first_webpage_request_datetime = IP[1][0] # First access time
                 last_webpage_request_datetime = IP[1][1] # Last access time
@@ -108,7 +132,7 @@ def main():
         # Increment the line index of log file by one
         index = index + 1
 
-
+    # Close the file handler of "sessionization.txt"
     sessinization_file_handler.close()
 
 
